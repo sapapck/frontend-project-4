5c6c8740aec3d75d600ffe5d18271197ad9e88d1
ismailibnaslanbek <isma.boss93@mail.ru>
2025-10-20 18:34:46 +0300

    cm

---
+++ frontend/src/componets/paje404.jsx
@@ -0,0 +1,15 @@
+const P404 = () => ( 
+    <body> 
+      <div class="error-container"> 
+        <h1> 404 </h1> 
+        <p> 
+          Oops! The page you're 
+          looking for is not here. 
+        </p> 
+        <a href="http://localhost:3000"> 
+          Go Back to Home 
+        </a> 
+      </div> 
+    </body> 
+  )
+  export default P404;
